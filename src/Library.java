9fc79f2dddbbb001c0284d83ad6a510afb0e6a97
Marcin <Majsyn@gmail.com>
2018-07-09 09:29:44 +0200

    Zmienne - aplikacja krok po kroku

---
+++ src/Library.java
@@ -0,0 +1,22 @@
+public class Library {
+    public static void main(String[] args) {
+        final String appName = "Biblioteka v0.1";
+
+        String title = "W pustyni i w puszczy";
+        String author = "Henryk Sienkiewicz";
+        int releaseDate = 2008;
+        int pages = 450;
+        String publisher = "Wydawnictwo Rupert";
+        String isbn = "23432543634";
+
+        System.out.println(appName);
+        System.out.println("Dostępne książki:");
+        System.out.println(title);
+        System.out.println(author);
+        System.out.println(releaseDate);
+        System.out.println(pages);
+        System.out.println(publisher);
+        System.out.println(isbn);
+    }
+}
+
