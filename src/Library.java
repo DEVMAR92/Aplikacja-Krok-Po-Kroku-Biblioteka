47303bef5db5e4c8afdd0b582d466f176608e9ae
Marcin <Majsyn@gmail.com>
2018-07-09 11:04:31 +0200

    Klasy i obiekty - aplikacja krok po kroku

--- src/Library.java
+++ src/Library.java
@@ -1,22 +1,23 @@
 public class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.1";
+        final String appName = "Biblioteka v0.2";
 
-        String title = "W pustyni i w puszczy";
-        String author = "Henryk Sienkiewicz";
-        int releaseDate = 2008;
-        int pages = 450;
-        String publisher = "Wydawnictwo Rupert";
-        String isbn = "23432543634";
+        Book book1 = new Book();
+        book1.title = "W pustyni i w puszczy";
+        book1.author = "Henryk Sienkiewicz";
+        book1.releaseDate = 2007;
+        book1.pages = 450;
+        book1.publisher = "Wydawnictwo Zbyszek";
+        book1.isbn = "21433253465";
 
         System.out.println(appName);
         System.out.println("Dostępne książki:");
-        System.out.println(title);
-        System.out.println(author);
-        System.out.println(releaseDate);
-        System.out.println(pages);
-        System.out.println(publisher);
-        System.out.println(isbn);
+        System.out.println(book1.title);
+        System.out.println(book1.author);
+        System.out.println(book1.releaseDate);
+        System.out.println(book1.pages);
+        System.out.println(book1.publisher);
+        System.out.println(book1.isbn);
     }
 }
 
