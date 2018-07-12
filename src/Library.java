b1eb1a5a25b4f8a132b0bd8962d052837707dad8
Marcin <Majsyn@gmail.com>
2018-07-12 12:23:00 +0200

    Metody i konstruktory - aplikacja krok po kroku

--- src/Library.java
+++ src/Library.java
@@ -1,23 +1,21 @@
 public class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.2";
+        final String appName = "Biblioteka v0.3";
 
-        Book book1 = new Book();
-        book1.title = "W pustyni i w puszczy";
-        book1.author = "Henryk Sienkiewicz";
-        book1.releaseDate = 2007;
-        book1.pages = 450;
-        book1.publisher = "Wydawnictwo Zbyszek";
-        book1.isbn = "21433253465";
+        Book book1 = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz",
+                2008, 450, "Nowa era", "3241324234423");
+        Book book2 = new Book("Dziady cz.III", "Adam Mickiewicz",
+                2004, 48, "Horyzont", "324324123123");
+        Book book3 = new Book("Ferdydurke", "Witold Gombrowicz",
+                2010, 255, "Johanes", "443455643234");
+
 
         System.out.println(appName);
-        System.out.println("Dostępne książki:");
-        System.out.println(book1.title);
-        System.out.println(book1.author);
-        System.out.println(book1.releaseDate);
-        System.out.println(book1.pages);
-        System.out.println(book1.publisher);
-        System.out.println(book1.isbn);
+        System.out.println("Książki dostępne w bibliotece:");
+        book1.printInfo();
+        book2.printInfo();
+        book3.printInfo();
+
     }
 }
 
