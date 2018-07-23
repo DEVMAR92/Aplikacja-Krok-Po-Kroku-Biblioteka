cac1333dc7aa70d75161793bb7f04e6bb8db91b9
Marcin <Majsyn@gmail.com>
2018-07-23 13:33:13 +0200

    v0.5

--- src/pl/javastart/library/app/Library.java
+++ src/pl/javastart/library/app/Library.java
@@ -4,21 +4,24 @@ import pl.javastart.library.model.Book;
 
 public class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.4";
+        final String appName = "Biblioteka v0.5";
 
-        Book book1 = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz",
+        Book[] books = new Book[1000];
+
+        books[0] = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz",
                 2008, 450, "Nowa era", "3241324234423");
-        Book book2 = new Book("Dziady cz.III", "Adam Mickiewicz",
+        books[1] = new Book("Dziady cz.III", "Adam Mickiewicz",
                 2004, 48, "Horyzont", "324324123123");
-        Book book3 = new Book("Ferdydurke", "Witold Gombrowicz",
+        books[2] = new Book("Ferdydurke", "Witold Gombrowicz",
                 2010, 255, "Johanes", "443455643234");
 
 
         System.out.println(appName);
-        System.out.println("Książki dostępne w bibliotece: \n");
-        book1.printInfo();
-        book2.printInfo();
-        book3.printInfo();
+        System.out.println("Książki dostępne w bibliotece:");
+        books[0].printInfo();
+        books[1].printInfo();
+        books[2].printInfo();
+        System.out.println("System może przechowywać " + books.length + " książek");
 
     }
 }
